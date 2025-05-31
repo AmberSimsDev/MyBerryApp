67dce4e8a83c651792ccb01aa3439063917230fc
Amber Sims <asdigitalassistant@gmail.com>
2025-05-31 18:09:53 +0200

    Completed Migration : It builds and runs, but crashes when clicked.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
   id("com.android.application")
     id("org.jetbrains.kotlin.android")
-   kotlin("plugin.serialization") version "2.0.21"
-
-
-
+   //kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -57,23 +54,7 @@ dependencies {
 
     //TODO fix pluggins and dependency issues
     //Navigation
-    val nav_version = "2.9.0"
-
-    // Jetpack Compose integration
-    implementation("androidx.navigation:navigation-compose:$nav_version")
-
-    // Views/Fragments integration
-    implementation("androidx.navigation:navigation-fragment:$nav_version")
-    implementation("androidx.navigation:navigation-ui:$nav_version")
-
-    // Feature module support for Fragments
-    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
-
-    // Testing Navigation
-    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
-
-    // JSON serialization library, works with the Kotlin serialization plugin
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
    //Navigation End
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
